96408da16e7d3e39eca4bfedfb6cd24ee3599d23
Nate Murray <nate@natemurray.com>
2011-05-04 16:23:26 -0700

    initial import

---
+++ src/cascading_simhash/core.clj
@@ -0,0 +1,1 @@
+(ns cascading-simhash.core)
---
+++ test/cascading_simhash/test/core.clj
@@ -0,0 +1,6 @@
+(ns cascading-simhash.test.core
+  (:use [cascading-simhash.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
