75c5c17455a58b6f902cac538f093dde4979169e
Nate Murray <nate@natemurray.com>
2011-05-04 16:26:59 -0700

    added project.clj and gitignore

--- project.clj
+++ project.clj
@@ -1,4 +1,17 @@
 (defproject cascading-simhash "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+  :description "Calculate simple simhashing in Hadoop"
+  :dependencies 
+  [[org.clojure/clojure "1.2.0"]
+   [org.clojure/clojure-contrib "1.2.0"]
+   [cascalog "1.7.0-SNAPSHOT"]
+   [net.htmlparser.jericho/jericho-html "3.1"]]
+  :dev-dependencies 
+  [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+   [swank-clojure "1.2.1"]]
+  :repositories
+  {"clojars" "http://clojars.org/repo"}
+  :jvm-opts ["-Xmx1400m"] 
+  :compile-path "build/classes"
+  :target-dir "build"
+  :java-source-path "src/java"
+  :source-path "src/clj")
