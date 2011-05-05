f3d4e2952b86af06768557aea70b764221a58171
Nate Murray <nate@natemurray.com>
2011-05-05 10:59:06 -0700

    updated comments in core

--- test/cascading_simhash/test/core.clj
+++ test/cascading_simhash/test/core.clj
@@ -1,6 +1,29 @@
-(ns cascading-simhash.test.core
-  (:use [cascading-simhash.core] :reload)
-  (:use [clojure.test]))
+
+(ns simhash.test.core
+  (:use 
+   [simhash core util]
+   [cascalog api testing]
+   [clojure.test])
+  (:require 
+   [simhash [taps :as t] [ops :as ops]]
+   [cascalog [workflow :as w] [vars :as v] [ops :as c]]))
+
+(def testfile "test-resources/test-documents.txt")
+
+(defn test-source [path]
+  (<- [?docid ?body]
+      ((hfs-textline path) ?line)
+      (ops/re-split-op [#"\t" 2] ?line :> ?docid ?body)
+      (:distinct false)))
+
+(comment
+
+ (?<- 
+  (stdout)
+  [?docid ?body]
+  ((test-source testfile) ?docid ?body))
+
+ )
 
 (deftest replace-me ;; FIXME: write
   (is false "No tests have been written."))
